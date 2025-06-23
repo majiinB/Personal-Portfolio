87fb80015615661c0f63b0898c587ddfe485ca01
Arthur Artugue <artuguearthur@gmail.com>
2025-06-23 20:14:17 +0800

    removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-neutral-bg">
-        <Analytics />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
